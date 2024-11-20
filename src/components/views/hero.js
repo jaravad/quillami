38bb4d9061d22f5e56566caeaccf383ae0396c71
Jesus Santiago <jesusdavid2607@gmail.com>
2024-11-20 15:08:58 -0500

    Add quote

--- src/components/views/hero.js
+++ src/components/views/hero.js
@@ -4,8 +4,8 @@ import { useEffect, useRef } from 'react'
 const Hero = () => {
   const videoRef = useRef(null)
 
-  useEffect(()=>{
-    if(videoRef.current) {
+  useEffect(() => {
+    if (videoRef.current) {
       videoRef.current.play()
     }
   }, [videoRef.current])
@@ -22,12 +22,9 @@ const Hero = () => {
         autoPlay
         ref={videoRef}
       >
-        <source
-          src="videos/carnaval-video-compressed.mp4"
-          type="video/mp4"
-        />
+        <source src="videos/carnaval-video-compressed.mp4" type="video/mp4" />
       </video>
-      <div className='bg-black/60 absolute w-full h-full -z-10'/>
+      <div className="bg-black/60 absolute w-full h-full -z-10" />
       <div className="max-w-lg w-4/5 logo flex flex-col items-center">
         <img src="images/logo.png" className="w-full" alt="logo" />
         <a
@@ -36,6 +33,11 @@ const Hero = () => {
         >
           Explora ahora
         </a>
+      </div>
+      <div className="absolute py-2.5 w-full bottom-0 bg-yellow-100">
+        <blockquote className="px-4 mx-auto container text-center italic max-w-2xl">
+          "Barranquilla en su punto, tierra de la cumbia y el sabor, donde se goza la vida con el alma y el corazón." "Barranquilla en su punto - <strong>Joe Arroyo</strong>"
+        </blockquote>
       </div>
     </div>
   )
